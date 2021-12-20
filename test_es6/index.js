f0017bb5d49c5bac1a35ea8f2d61375618b7fd2e
Franco Abosso <franco.abosso@gm2dev.com>
2021-12-20 10:16:24 -0300

    retry 3

--- test_es6/index.js
+++ test_es6/index.js
@@ -1,5 +1,4 @@
 import { SQSClient, AddPermissionCommand } from "@aws-sdk/client-sqs";
-
 const handler = async (event, context) => {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
   return context.logStreamName;
