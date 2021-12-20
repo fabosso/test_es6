f4cecae0b5117b18dcfe93d0b211115ad08d0e36
Franco Abosso <franco.abosso@gm2dev.com>
2021-12-20 10:06:32 -0300

    testing workflow

--- test_es6/index.js
+++ test_es6/index.js
@@ -1,5 +1,7 @@
 import { SQSClient, AddPermissionCommand } from "@aws-sdk/client-sqs";
 
+//asdasd
+
 const handler = async (event, context) => {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
   return context.logStreamName;
