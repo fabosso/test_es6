1de707f57d20b2bb856e37fc9dd72d2ae89d7fa7
fabosso <81111245+fabosso@users.noreply.github.com>
2021-12-20 09:43:02 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import { SQSClient, AddPermissionCommand } from "@aws-sdk/client-sqs";
+
+const handler = async (event, context) => {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  return context.logStreamName
+};
+
+export {
+    handler,
+}
